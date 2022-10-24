16df8733d076577f2692f1d6b94a4188687063ab
danmatuoka <dan_mtk@hotmail.com>
2022-10-24 00:18:11 -0300

    quase finalizado

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,20 +1,11 @@
 import { Request, Response } from "express";
 import { IScheduleRequest } from "../interfaces/schedules";
 import { createScheduleService, listScheduleService } from "../services/schedule.service";
-import jwt from "jsonwebtoken"
 
 
 const createSchedulesController = async (req: Request, res: Response) => {
 
-      const { propertyId, date, hour }: IScheduleRequest = req.body;
-
-      const { authorization } = req.headers
-
-    const token = authorization!.split(" ")[1]
-
-    const { id } = jwt.decode(token) as { id: string }
-
-    const userId = id
+      const { propertyId, date, hour, userId }: IScheduleRequest = req.body;
   
       const schedule = await createScheduleService({
         userId,
